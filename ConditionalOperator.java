920f05ea95d1767700f8ed84b2674b8be7678555
saichandu-513 <saichandugovindalapudi@gmail.com>
2025-06-17 14:19:30 +0530

    Add files via upload

---
+++ ConditionalOperator.java
@@ -0,0 +1,10 @@
+class ConditionalOperator
+{
+public static void main(String[] args)
+{
+int a=5;
+int b=6;
+System.out.println((a%2==0) ? "Even":"odd");
+System.out.println((b%2==0) ? "Even":"odd");
+}
+}
