38796ad4836e0c6e3af2f028de0666cf9d815d44
saichandu-513 <saichandugovindalapudi@gmail.com>
2025-06-19 14:27:32 +0530

    MethodsBasics

---
+++ MethodBasics.java
@@ -0,0 +1,57 @@
+/*class Method1
+{
+public void add()
+{
+System.out.println(10+20);
+}
+public static void main(String[] args)
+{
+Method1 m=new Method1();
+m.add();
+}
+}*/
+
+/*class Method2
+{
+public int sub()
+{
+return(40-30);
+}
+public static void main(String[] args)
+{
+Method2 m=new Method2();
+int c=m.sub();
+System.out.println(c);
+}
+}*/
+
+/*class Method3
+{
+public void mul(int a,int b)
+{
+System.out.println(a*b);
+}
+public static void main(String[] args)
+{
+Method3 m=new Method3();
+m.mul(10,20);
+}
+}*/
+
+/*class Method4
+{
+public int div(int a,int b)
+{
+return(a/b);
+}
+public static void main(String[] args)
+{
+Method4 m=new Method4();
+int c=m.div(40,5);
+System.out.println(c);
+}
+}*/
+
+
+
+
